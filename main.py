75cef891e312788a9de78cfc4b917a95bc43a1b2
Stephen Shaw <stephen.shaw@softwire.com>
2019-08-06 17:46:20 +0100

    Implement Part 2.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import requests
 import json
 
+def get_application_keys():
+    with open('secrets.txt') as f:
+        data = f.read().split("\n")
+    # Either substitute your API keys here or write a secrets.txt file with the ID in the first line and key in the second
+    return {'app_id': data[0], 'app_key': data[1]}
+
+class TransportAPI:
+    def __init__(self):
+        api_keys = get_application_keys()
+        self.payload = api_keys
+
+    def get_stopcodes_nearest(self, latitude, longitude):
+        stops_data_response = requests.get(f'http://transportapi.com/v3/uk/places.json?lat={latitude}&lon={longitude}&type=bus_stop', params=self.payload)
+        stops_data = json.loads(stops_data_response.text)
+        nearest_stopcodes = [stop['atcocode'] for stop in stops_data['member']]
+        return nearest_stopcodes
+
+    def get_live_bus_stop(self, atcocode):
+        stop_data_response = requests.get(f'http://transportapi.com/v3/uk/bus/stop/{atcocode}/live.json', params=self.payload)
+        stop_data = json.loads(stop_data_response.text)
+        stop = LiveBusStop(stop_data)
+        return stop
+
 class LiveBusStop:
     def __init__(self, data):
         self.data = data
@@ -26,23 +49,41 @@ class Bus:
     def to_string(self):
         return f"{self.departs()} - {self.line_name()}"
 
-def get_application_keys():
-    with open('secrets.txt') as f:
-        data = f.read().split("\n")
-    # Either substitute your API keys here or write a secrets.txt file with the ID in the first line and key in the second
-    return {'app_id': data[0], 'app_key': data[1]}
+class PostcodeAPI:
+    def get_postcode(self, postcode):
+        postcode_data_response = requests.get(f'http://api.postcodes.io/postcodes/{postcode}')
+        postcode_data = json.loads(postcode_data_response.text)
+        if postcode_data['status'] != 200:
+            return None
+        postcode = Postcode(postcode_data)
+        return postcode
+
+class Postcode:
+    def __init__(self, data):
+        self.data = data['result']
+    
+    def longitude(self):
+        return self.data['longitude']
+
+    def latitude(self):
+        return self.data['latitude']
 
 def main():
-    api_keys = get_application_keys()
+    postcodeApi = PostcodeAPI()
+    transportApi = TransportAPI()
 
     print('Welcome to BusBoard.')
-    atcocode = input('Please input a stopcode: ')
-    payload = api_keys
-    stop_data_response = requests.get(f'http://transportapi.com/v3/uk/bus/stop/{atcocode}/live.json', params=payload)
-    stop_data = json.loads(stop_data_response.text)
-    stop = LiveBusStop(stop_data)
+    postcode_string = input('Please input a postcode: ')
+    postcode = postcodeApi.get_postcode(postcode_string)
+    if postcode == None:
+        print("Bad postcode.")
+        return
+    stopcodes = transportApi.get_stopcodes_nearest(postcode.latitude(), postcode.longitude())
 
-    print('These are the upcoming buses at ' + stop.name() + ':')
-    print(stop.next_departures())
+    for stopcode in stopcodes[0:2]:
+        stop = transportApi.get_live_bus_stop(stopcode)        
+        print('These are the upcoming buses at ' + stop.name() + ':')
+        print(stop.next_departures())
+        print()
 
 if __name__ == "__main__": main()
