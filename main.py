ae3ab3fe5feff0f253ae25d97d821f126c0ccd60
SAUNDERS Rowan <rsaunders@altranuk.local>
2019-09-16 10:33:07 +0100

    Initialised Project, set up url requests for Transport API

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import requests
+
 def main():
     print("Welcome to BusBoard.")
+    # atcocode = input("Please enter a bus stop code: \n")
+    atcocode = "0180BAC30592"
+    with open("app_id.txt", "r") as file:
+        app_id = file.read()
+    with open("app_key.txt", "r") as file:
+        app_key = file.read()
+    r = requests.get(
+        "https://transportapi.com/v3/uk/bus/stop/"
+        + atcocode +  # atcocode
+        "/live.json?app_id=" + app_id +  # app_id
+        "&app_key=" + app_key +  # app_key
+        "&group=" + "route" +  # group departures by route ("route") or return one group ("no")w
+        "&integer=" + "5" +  # number of departures
+        "&nextbuses=" + "yes"
+    )
+    json_file = r.json()
+    print(json_file)
+
 
 
 if __name__ == "__main__": main()
