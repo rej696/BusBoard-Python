a38a74f5ba25705a08a880f55ab0ef7f708fdcb6
SAUNDERS Rowan <rsaunders@altranuk.local>
2019-09-16 15:24:04 +0100

    Formated code to use classes

--- main.py
+++ main.py
@@ -2,31 +2,47 @@ import requests
 import json
 
 
-def departures_json_decoder(bus_data_json):
-    bus_data_all = bus_data_json.json()
-    bus_data = bus_data_all["departures"]["all"]
-    departures_dict = []
-    for index in range(len(bus_data)):
-        departures_dict.append({
-            "Line Name": bus_data[index]["line_name"],
-            "Aimed Departure Time": bus_data[index]["aimed_departure_time"],
-            "Expected Departure Time": bus_data[index]["best_departure_estimate"],
-            "Final Destination": bus_data[index]["direction"]
-        })
-    return departures_dict
+class SingleDeparture:
+    def __init__(self, line_name, aimed_departure_time, expected_departure_time, final_destination):
+        self.line_name = line_name
+        self.aimed_departure_time = aimed_departure_time
+        self.expected_departure_time = expected_departure_time
+        self.final_destination = final_destination
+
+    def __repr__(self):
+        single_departure = "\nLine Name : " + self.line_name + "\nAimed Departure Time : " + self.aimed_departure_time +\
+                           "\nExpected Departure Time : " + self.expected_departure_time +\
+                           "\nFinal Destination : " + self.final_destination
+        return single_departure
+
+    @staticmethod
+    def json_decoder(single_bus_departure_json):
+        return SingleDeparture(
+            single_bus_departure_json["line_name"], single_bus_departure_json["aimed_departure_time"],
+            single_bus_departure_json["best_departure_estimate"], single_bus_departure_json["direction"]
+        )
 
 
-def bus_stop_json_decoder(bus_data_json):
-    bus_data_all = bus_data_json.json()
-    bus_stop_data = {
-        "Stop Name": bus_data_all["stop_name"],
-    }
-    return bus_stop_data
+class BusDepartures:
+    def __init__(self, bus_stop_name, buses):
+        self.bus_stop_name = bus_stop_name
+        self.buses = buses
 
+    @staticmethod
+    def json_decoder(json_file):
+        bus_stop_name = json_file["stop_name"]
+        bus_departures_data = json_file["departures"]["all"]
+        departures = []
+        single_departure = SingleDeparture
+        for index in range(len(bus_departures_data)):
+            departures.append(single_departure.json_decoder(bus_departures_data[index]))
+        return BusDepartures(bus_stop_name, departures)
 
-def display_bus_data(bus_data):
-    for key in bus_data:
-        print(key + ": ", bus_data[key])
+    def __repr__(self):
+        buses_string = []
+        for bus in self.buses:
+            buses_string.append(str(bus))
+        return ("\n\nBus Stop Name : " + str(self.bus_stop_name)).join(buses_string)
 
 
 def main():
@@ -38,7 +54,7 @@ def main():
         app_id = file.read()
     with open("app_key.txt", "r") as file:
         app_key = file.read()
-    bus_data_json = requests.get(
+    bus_stop_data_json = requests.get(
         "https://transportapi.com/v3/uk/bus/stop/"
         + atcocode +  # atcocode
         "/live.json?app_id=" + app_id +  # app_id
@@ -46,15 +62,9 @@ def main():
         "&group=" + "no" +  # group departures by route ("route") or return one group ("no")w
         "&limit=" + "5" +  # number of departures
         "&nextbuses=" + "no"
-    )
-    # with open("bus_data.json", "r") as file:
-    #     bus_data_json = file.read()
-    bus_stop_data_decoded = bus_stop_json_decoder(bus_data_json)
-    display_bus_data(bus_stop_data_decoded)
-    departures_dict = departures_json_decoder(bus_data_json)
-    for index in range(len(departures_dict)):
-        print("\nBus Number " + str(index + 1))
-        display_bus_data(departures_dict[index])
+    ).json()
+    bus_departures = BusDepartures.json_decoder(bus_stop_data_json)
+    print(bus_departures)
 
 
 if __name__ == "__main__": main()
