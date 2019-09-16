cb7dc943a1881154bd3f0f7fd21f4f3ffd9bc0cb
SAUNDERS Rowan <rsaunders@altranuk.local>
2019-09-16 12:27:24 +0100

    Included Bus Departures for next 5 buses at stop

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import requests
+import json
+
+
+def departures_json_decoder(bus_data_json):
+    bus_data_all = bus_data_json.json()
+    bus_data = bus_data_all["departures"]["all"]
+    departures_dict = []
+    for index in range(len(bus_data)):
+        departures_dict.append({
+            "Line Name": bus_data[index]["line_name"],
+            "Aimed Departure Time": bus_data[index]["aimed_departure_time"],
+            "Expected Departure Time": bus_data[index]["best_departure_estimate"],
+            "Final Destination": bus_data[index]["direction"]
+        })
+    return departures_dict
+
+
+def bus_stop_json_decoder(bus_data_json):
+    bus_data_all = bus_data_json.json()
+    bus_stop_data = {
+        "Stop Name": bus_data_all["stop_name"],
+    }
+    return bus_stop_data
+
+
+def display_bus_data(bus_data):
+    for key in bus_data:
+        print(key + ": ", bus_data[key])
+
 
 def main():
     print("Welcome to BusBoard.")
     # atcocode = input("Please enter a bus stop code: \n")
-    atcocode = "0180BAC30592"
+    atcocode = "0180BAA01336"  # Kelston View (The Hollow)
+    # atcocode = "0180BAC30302"  # Lorne Road
     with open("app_id.txt", "r") as file:
         app_id = file.read()
     with open("app_key.txt", "r") as file:
         app_key = file.read()
-    r = requests.get(
+    bus_data_json = requests.get(
         "https://transportapi.com/v3/uk/bus/stop/"
         + atcocode +  # atcocode
         "/live.json?app_id=" + app_id +  # app_id
         "&app_key=" + app_key +  # app_key
-        "&group=" + "route" +  # group departures by route ("route") or return one group ("no")w
-        "&integer=" + "5" +  # number of departures
-        "&nextbuses=" + "yes"
+        "&group=" + "no" +  # group departures by route ("route") or return one group ("no")w
+        "&limit=" + "5" +  # number of departures
+        "&nextbuses=" + "no"
     )
-    json_file = r.json()
-    print(json_file)
-
+    # with open("bus_data.json", "r") as file:
+    #     bus_data_json = file.read()
+    bus_stop_data_decoded = bus_stop_json_decoder(bus_data_json)
+    display_bus_data(bus_stop_data_decoded)
+    departures_dict = departures_json_decoder(bus_data_json)
+    for index in range(len(departures_dict)):
+        print("\nBus Number " + str(index + 1))
+        display_bus_data(departures_dict[index])
 
 
 if __name__ == "__main__": main()
