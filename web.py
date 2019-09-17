12e05fef7a3abeb2033114a1efb4a6f16572ee48
SAUNDERS Rowan <rsaunders@altranuk.local>
2019-09-17 17:06:51 +0100

    both web and command line applications are functional

--- web.py
+++ web.py
@@ -10,15 +10,14 @@ def index():
 
 
 @app.route("/busInfo")
-def bus_info():
+def BusInfo():
     postcode = request.args.get('postcode')
-    number_of_stops = request.args.get('number_of_stops')
+    number_of_stops = int(request.args.get('number_of_stops'))
     number_of_buses = request.args.get('number_of_buses')
     postcode_travel_info = PostcodeTravelInfo(postcode, number_of_stops, number_of_buses)
-    line_name = postcode_travel_info.bus_stops[0].departures[0].line_name
     return render_template(
         'info.html', postcode=postcode, number_of_stops=number_of_stops, number_of_buses=number_of_buses,
-        line_name=line_name
+        postcode_travel_info=postcode_travel_info
     )
 
 
